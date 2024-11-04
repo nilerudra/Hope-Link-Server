7a418d601d96a92e026d1bcd2b591c2a58f17da4
Rudra <rudrakshnile930@gmail.com>
2024-11-04 15:38:02 +0530

    issue resolved

--- src/routes/explore.js
+++ src/routes/explore.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Ngo = require("../models/ngodetails");
+const Ngo = require("../models/ngo");
 
+router.get("/", async (req, res) => {
+  const users = await Ngo.find({ typeOfNGO: "Water" });
+  console.log(users);
+  res.json(users); // Send JSON data
+});
 
-
-  router.get('/', async(req, res) => {
-    const users = await Ngo.find({typeOfNGO:"Water"});
-    console.log(users);
-    res.json(users);  // Send JSON data
-  });
-
-   
 module.exports = router;
